43167beb0bd023fec095bc4712fbeac507860641
fiqryq <fiqrychoerudin48@gmail.com>
2022-07-30 20:44:15 +0700

    Initial Commit

---
+++ SwiftUI-Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Weather
+//
+//  Created by Fiqry Choerudin on 30/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Fiqry Choerudin on 30/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
